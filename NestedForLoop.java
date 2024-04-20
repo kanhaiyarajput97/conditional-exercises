1b4002d92ec0d06df140fb680e44cd72e5a74faf
kanhaiyarajput97 <kanhaiaysingh3197@gmail.com>
2024-04-20 18:24:28 +0530

    Nested for loop ex

--- NestedForLoop.java
+++ NestedForLoop.java
@@ -2,8 +2,8 @@ public class NestedForLoop {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i <= 10; i++) {
-            for (int p = 0; p < i+2; p++) {
+        for (int i = 0; i < 5; i++) {
+            for (int p = 0; p < i+1; p++) {
                 System.out.print("*");
             }
             System.out.println();
